72685b3265ed93078aa9ea4f08a10bc300eed87c
Alejandro <alesantin012@hotmail.com>
2021-02-25 16:54:33 -0300

    barrier concurrency pattern

---
+++ barrier-concurrency-pattern/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+var timeoutMilliseconds int = 5000
+var endpoints = []string{
+	"https://jsonplaceholder.typicode.com/posts/1",
+	"https://jsonplaceholder.typicode.com/posts/2",
+	"https://jsonplaceholder.typicode.com/posts/3",
+	"https://jsonplaceholder.typicode.com/posts/4",
+	"https://jsonplaceholder.typicode.com/posts/5",
+	"https://jsonplaceholder.typicode.com/posts/7",
+	"https://jsonplaceholder.typicode.com/posts/8",
+	"https://jsonplaceholder.typicode.com/posts/9",
+	"https://jsonplaceholder.typicode.com/posts/10",
+}
+
+//BarrierResp ..
+type BarrierResp struct {
+	Err  error
+	Resp string
+}
+
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Catch exception: ", r)
+		}
+	}()
+	responses := barrier(endpoints)
+	fmt.Println(responses)
+}
+
+func barrier(endpoints []string) []BarrierResp {
+
+	requestNumber := len(endpoints)
+	in := make(chan BarrierResp, requestNumber)
+	defer close(in)
+
+	responses := make([]BarrierResp, requestNumber)
+	for _, endpoint := range endpoints {
+		go makeRequest(in, endpoint)
+	}
+
+	var hasError bool
+	for i := 0; i < requestNumber; i++ {
+		resp := <-in
+		if resp.Err != nil {
+			fmt.Println("ERROR: ", resp.Err)
+			hasError = true
+		}
+		responses[i] = resp
+	}
+
+	if hasError {
+		panic("Invalid Response")
+	}
+
+	return responses
+
+}
+
+func makeRequest(out chan<- BarrierResp, url string) {
+	res := BarrierResp{}
+	client := http.Client{
+		Timeout: time.Duration(time.Duration(timeoutMilliseconds) *
+			time.Millisecond),
+	}
+	resp, err := client.Get(url)
+	if err != nil {
+		res.Err = err
+		out <- res
+		return
+	}
+	byt, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		res.Err = err
+		out <- res
+		return
+	}
+	res.Resp = string(byt)
+	out <- res
+}
